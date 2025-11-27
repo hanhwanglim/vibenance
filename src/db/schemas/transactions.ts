3024631be9e8d11114b35054a94c8b863a07467e
Han Hwang Lim <hanhwanglim@gmail.com>
2025-11-27 21:18:06 +0000

    feat: normalise account and categories

--- src/db/schemas/transactions.ts
+++ src/db/schemas/transactions.ts
@@ -4,8 +4,29 @@ import {
   timestamp,
   numeric,
   AnyPgColumn,
+  unique,
 } from "drizzle-orm/pg-core";
 import { user } from "./auth";
+
+export const bankAccount = pgTable("bank_account", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull().unique(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull(),
+});
+
+export const category = pgTable("category", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull().unique(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull(),
+});
 
 export const transaction = pgTable("transaction", {
   id: text("id").primaryKey(),
@@ -13,12 +34,16 @@ export const transaction = pgTable("transaction", {
     .notNull()
     .references(() => user.id, { onDelete: "cascade" }),
   transactionHash: text("transaction_hash").notNull().unique(),
-  account: text("account").notNull(),
+  accountId: text("account_id")
+    .notNull()
+    .references(() => bankAccount.id),
   timestamp: timestamp("timestamp").notNull(),
   name: text("name").notNull(),
   currency: text("currency").notNull(),
   amount: numeric("amount", { precision: 50, scale: 18 }).notNull(),
-  category: text("category").notNull(),
+  categoryId: text("category_id")
+    .notNull()
+    .references(() => category.id),
   reference: text("reference"),
   notes: text("notes"),
   parentTransactionId: text("parent_transaction_id").references(
@@ -33,3 +58,5 @@ export const transaction = pgTable("transaction", {
 });
 
 export type TransactionInsert = typeof transaction.$inferInsert;
+export type BankAccountInsert = typeof bankAccount.$inferInsert;
+export type CategoryInsert = typeof category.$inferInsert;
