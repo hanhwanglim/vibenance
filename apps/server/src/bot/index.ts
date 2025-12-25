f8382f548b215c237da7c9afeda815117a85d1b0
Han Hwang Lim <hanhwanglim@gmail.com>
2025-12-25 22:31:59 +0000

    fix: add error handling to bot

--- apps/server/src/bot/index.ts
+++ apps/server/src/bot/index.ts
@@ -1,4 +1,4 @@
-import { Bot } from "grammy";
+import { Bot, GrammyError, HttpError } from "grammy";
 import { handleDocuments, handleImport, help, start } from "./handlers";
 import { logging } from "./middleware";
 
@@ -17,3 +17,16 @@ bot.command("help", help);
 
 bot.on("message:document", handleDocuments);
 bot.callbackQuery(/^import:(\d+):(\d+)$/, handleImport);
+
+bot.catch((err) => {
+	const ctx = err.ctx;
+	console.error(`Error while handling update ${ctx.update.update_id}:`);
+	const e = err.error;
+	if (e instanceof GrammyError) {
+		console.error("Error in request:", e.description);
+	} else if (e instanceof HttpError) {
+		console.error("Could not contact Telegram:", e);
+	} else {
+		console.error("Unknown error:", e);
+	}
+});
